4ac9460e1a36a5b4662d43a9d501fb0e5dec7556
JJKKJJKJ <spergals950814@gmail.com>
2025-02-27 12:05:29 +0900

    git연결

---
+++ jihye/src/main/java/com/qwer/JihyeApplication.java
@@ -0,0 +1,13 @@
+package com.qwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JihyeApplication.class, args);
+	}
+
+}
